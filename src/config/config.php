0f635ed1ea447d40037d167c2006e49c7fea6f32
Bruce <lihong@artxun.com>
2018-01-25 09:56:26 +0800

    Init

---
+++ src/config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by Bruce.
+ * 阿里云开放式搜索配置文件
+ * User: Bruce
+ * Date: 2018/1/24
+ * Time: 17:59
+ */
+
+ return [
+         'ACCESS_KEY_ID' => '',
+         'ACCESS_KEY_SECRET' => '',
+     //替换为对应区域api访问地址，可参考应用控制台,基本信息中api地址
+         'ENDPOINT' => 'http://opensearch-cn-qingdao.aliyuncs.com',
+     //开启调试模式
+         'options' => array('debug' => true),
+     //替换为应用名
+         'app_name' => '',
+     //默认索引名称
+         'index_name' => '',
+     //替换为下拉提示名称
+         'suggest_name' => '',
+     //查询分析
+         'app_qp' => '',
+ ];
