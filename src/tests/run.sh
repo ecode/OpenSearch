a61b75dc944c16bc80eb34bd586a2913225828fb
Bruce <lihong@artxun.com>
2020-03-24 14:33:42 +0800

    更新阿里云 SDK 至 V3.2.1

--- src/tests/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/bash
-
-CURRENT_DIR=`dirname $0`
-cd $CURRENT_DIR
-
-chmod 777 ./phpunit-4.7.0
-
-#./phpunit-4.7.0  --bootstrap ./Bootstrap.php  --colors --verbose  --debug  unittest/Client/BehaviorCollectionClientTest
-./phpunit-4.7.0  --bootstrap ./Bootstrap.php  --colors --verbose  --debug  unittest/Client/SearchClientTest
